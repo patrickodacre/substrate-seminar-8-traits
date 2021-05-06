e06ef83bad6ef3c933fa33185cd19e937d51c18a
pwho <patrick@patrickwho.me>
2021-05-06 09:07:57 -0400

    Impl basic operations on Balances Module

---
+++ src/balances.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+pub struct BalancesModule
+{
+    balances: HashMap<u32, u32>,
+}
+
+impl BalancesModule
+{
+    pub fn new() -> Self
+    {
+        Self {
+            balances: HashMap::new(),
+        }
+    }
+
+    pub fn balance(&self, who: u32) -> u32
+    {
+        *self.balances.get(&who).unwrap_or(&0)
+    }
+
+    pub fn set_balance(&mut self, who: u32, amount: u32)
+    {
+        self.balances.insert(who, amount);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-fn main()
+mod balances;
+
+fn main() {}
+
+#[test]
+fn test_set_balance()
 {
-    println!("Hello, World!");
+    let alice = 1;
+
+    let mut bals = balances::BalancesModule::new();
+    assert_eq!(bals.balance(alice), 0);
+
+    bals.set_balance(alice, 10);
+    assert_eq!(bals.balance(alice), 10);
 }
